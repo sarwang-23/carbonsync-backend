213c42488755ea819bbe0b1f0370281d01945ecb
carbonsync-prog <sarwang.carbonsync@gmail.com>
2026-07-08 23:24:48 +0530

    zx

--- src/services/climatiq.service.ts
+++ src/services/climatiq.service.ts
@@ -394,7 +394,6 @@ export async function estimateWithClimatiqDirect(
   if (!input.parameters && input.parameterUnit) {
     body.parameters[`${input.parameterName}_unit`] = input.parameterUnit;
   }
-  console.log("[estimateWithClimatiqDirect] PAYLOAD:", JSON.stringify(body));
 
   const response = await fetch(`${CLIMATIQ_BASE_URL}/estimate`, {
     method: "POST",
